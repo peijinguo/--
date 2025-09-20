91d31268047d27d707aabd091abbf3e09e3b7320
peijinguo <pufffguo@gmail.com>
2025-09-20 22:00:40 +0800

    Add responsive CSS styles for header and menu layout
    - Created a new style.css file for the layout of the header and menu.
    - Implemented a responsive design that adjusts the menu display for screens smaller than 760px.
    - Added styles for the logo, header, and content sections.
    - Included hover effects for menu items and a toggle for the mobile menu.

---
+++ RWD/15 漢堡選單/all.js
@@ -0,0 +1,8 @@
+$(document).ready(
+   function () {
+      $('.showmenu').on('click', function (e) {
+         e.preventDefault();
+         $('body').toggleClass('menu-show');
+      });
+   }
+);
